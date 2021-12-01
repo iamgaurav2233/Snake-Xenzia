7d9aa622c5606f7b96984764b3abc43c32c887e1
Gaurav dube <90771453+iamgaurav2233@users.noreply.github.com>
2021-12-01 17:11:34 +0530

    Update GameFrame.java

--- files/GameFrame.java
+++ files/GameFrame.java
@@ -4,16 +4,13 @@ public class GameFrame extends JFrame{
 
 	GameFrame(){
 			
-		this.add(new GamePanel());
-
+	this.add(new GamePanel());
 	this.setTitle("Snake");
-		 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	this.setResizable(false);
-		
-    this.pack();
-		
-  this.setVisible(true);
-		this.setLocationRelativeTo(null);
+	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	this.setResizable(false);	
+        this.pack();
+	this.setVisible(true);
+        this.setLocationRelativeTo(null);
 		
 	}
 }
